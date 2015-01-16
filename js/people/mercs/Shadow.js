b5e000d096aff3346123798d65cc28041c45fb3a
misantronic <david.skx@posteo.de>
2015-01-16 11:18:48 +0100

    param to equip merc with standard gear

--- js/people/mercs/Shadow.js
+++ js/people/mercs/Shadow.js
@@ -1,4 +1,4 @@
-function Shadow() {
+function Shadow(equip) {
 	Merc.call(this, {
 		health: 95,
 		agility: 96,
@@ -12,6 +12,22 @@ function Shadow() {
 		mecanical: 35,
 		medical: 30
 	}, 'Kyle Simmons', 'Shadow');
+
+	if(equip) {
+		var helmet 	= new SteelHelmet();
+		var west 	= new KevlarWest();
+		var shorts 	= new KevlarShorts();
+		var colt 	= new Colt_Commando();
+
+		this
+			.equip(helmet)
+			.equip(west)
+			.equip(shorts)
+			.equip(colt)
+			.equip(new $5_56MM())
+			.equip(new $5_56MM())
+			.grab(colt);
+	}
 }
 
 Shadow.prototype = new Merc();
