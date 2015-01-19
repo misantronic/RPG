7469ae4f0cfa4a8a3bb0fcefa41bf1bb3c90a7c2
misantronic <david.skx@posteo.de>
2015-01-19 16:36:06 +0100

    added mp5k

---
+++ js/items/weapons/smg/MP5K.js
@@ -0,0 +1,17 @@
+function MP5K() {
+	Weapon.call(this, Weapon.SMG, {
+		name: 'H&K MP5K',
+		thumb: './img/weapons/mp5k.jpg',
+		damage: 23,
+		ammo: $9x19MM,
+		magazine: 30,
+		range: 20,
+		ap: [ 6, 10 ],
+		weight: 2.1,
+		size: Item.MEDIUM,
+		hands: 1
+	});
+}
+
+MP5K.prototype = new Weapon();
+MP5K.constructor = MP5K;
