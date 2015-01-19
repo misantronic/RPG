6b4388ab1fca1385a6865bf3564850a755a7f86d
misantronic <david.skx@posteo.de>
2015-01-19 23:59:41 +0100

    added rounds and properties

--- js/items/Ammo.js
+++ js/items/Ammo.js
@@ -1,6 +1,6 @@
 /**
  *
- * @param {String} caliber {Ammo.$9x19MM}
+ * @param {String} caliber {Ammo.$9x19MM|Ammo.$5_45MM|Ammo.$5_56MM}
  * @param {Object} [stats]
  * @constructor
  */
@@ -27,7 +27,7 @@ Ammo.constructor = Ammo;
 Ammo.prototype._caliber = '';
 Ammo.prototype._type = '';
 Ammo.prototype._stats = {
-
+	rounds: 0
 };
 
 Ammo.$9x19MM 	= '9x19mm';
@@ -38,3 +38,32 @@ Ammo.TYPE_AP 		= 'ap';		// armor piercing
 Ammo.TYPE_HP 		= 'hp';		// hollow point
 Ammo.TYPE_GLASER 	= 'glaser'; // glaser
 Ammo.TYPE_BALL 		= 'ball';	// standard ball ammo
+
+Ammo.PROPERTIES = {
+	ap		: [ 1.3, 1, 1 ],
+	hp		: [ 0.7, 1.7, 1 ],
+	glaser	: [ 0.3, 3, 1 ],
+	ball	: [ 1, 1, 1 ]
+};
+
+// GETTER / SETTER
+
+Object.defineProperty(Ammo.prototype, "rounds", {
+	get: function () {
+		return this._stats.rounds;
+	},
+
+	set: function(val) {
+		this._stats.rounds = val;
+	}
+});
+
+Object.defineProperty(Ammo.prototype, "caliber", {
+	get: function () {
+		return this._caliber;
+	},
+
+	set: function(val) {
+		this._caliber = val;
+	}
+});
