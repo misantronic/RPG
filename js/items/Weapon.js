ced9235f0247b7ec097e0b4f1dfdc1fd192e95be
misantronic <david.skx@posteo.de>
2015-01-20 14:29:29 +0100

    added burst

--- js/items/Weapon.js
+++ js/items/Weapon.js
@@ -8,11 +8,34 @@ function Weapon(type, stats) {
 	if(!type) return;
 
 	// reset
-	this._stats = {};
+	this._stats = {
+		damage	: 0,
+		caliber	: Ammo,
+		magazine: 0,
+		range	: 0,
+		ap		: [ 0 ],
+		burst	: false
+	};
 
 	this._type = type;
 
 	if(stats) {
+
+		// burst mode
+		if(stats.burst === undefined) {
+			switch(type) {
+				case Weapon.PISTOL:
+				case Weapon.RIFLE:
+					stats.burst = false;
+					break;
+				case Weapon.SMG:
+				case Weapon.ASSAULT_RIFLE:
+					stats.burst = true;
+					break;
+				default:
+			}
+		}
+
 		for(var stat in stats) {
 			if(stats.hasOwnProperty(stat)) {
 				this[stat] = stats[stat];
@@ -24,21 +47,24 @@ function Weapon(type, stats) {
 Weapon.prototype = new Item();
 Weapon.constructor = Weapon;
 
-Weapon.prototype._caliber = Ammo;
 Weapon.prototype._ammo = Ammo;
 
 Weapon.prototype._stats = {
-	damage: 0,
-	caliber: Ammo,
+	damage	: 0,
+	caliber	: Ammo,
 	magazine: 0,
-	range: 0,
-	ap: [ 0 ]
+	range	: 0,
+	ap		: [ 0 ],
+	burst	: false
 };
 
 Weapon.PISTOL 			= 'pistol';
 Weapon.SMG 				= 'smg';
 Weapon.RIFLE 			= 'rifle';
 Weapon.ASSAULT_RIFLE 	= 'assault_rifle';
+
+Weapon.MODE_SINGLE 	= 'single';
+Weapon.MODE_BURST 	= 'burst';
 
 /**
  *
@@ -113,5 +139,15 @@ Object.defineProperty(Weapon.prototype, "ap", {
 
 	set: function(val) {
 		this._stats.ap = val;
+	}
+});
+
+Object.defineProperty(Weapon.prototype, "burst", {
+	get: function () {
+		return this._stats.burst;
+	},
+
+	set: function(val) {
+		this._stats.burst = val;
 	}
 });
