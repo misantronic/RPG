ff4cf6cea94ab51f3ad982d57502f14c53f08f25
misantronic <david.skx@posteo.de>
2015-01-20 17:24:46 +0100

    moved js from Game.html to Game.js

--- js/Game.js
+++ js/Game.js
@@ -1,7 +1,74 @@
 function Game() {
+	Minc([
+			'./bower_components/300494/mersenne-twister.js',
 
+			'js/Point.js',
+			'js/Abstract.js',
+			'js/world/World.js',
+
+			// items
+			'js/items/Item.js',
+			'js/items/Wear.js',
+			'js/items/Ammo.js',
+			'js/items/Weapon.js',
+
+			// people
+			'js/people/Person.js',
+			'js/people/Merc.js',
+			'js/people/mercs/Shadow.js',
+			'js/people/mercs/Ivan.js',
+
+			// Wear
+			'js/items/wear/head/SteelHelmet.js',
+			'js/items/wear/body/KevlarWest.js',
+			'js/items/wear/legs/KevlarShorts.js',
+
+			// Ammo
+			'js/items/ammo/9mm.js',
+			'js/items/ammo/5.56mm.js',
+			'js/items/ammo/5.45mm.js',
+
+			// Weapons
+			'js/items/weapons/pistol/Beretta_92F.js',
+			'js/items/weapons/assault_rifle/Colt_Commando.js',
+			'js/items/weapons/smg/MP5K.js',
+			'js/items/weapons/assault_rifle/AK-74.js'
+		]
+	).done(this._init.bind(this));
+
+	location.hash = "";
+	window.onhashchange = this._onHashChange.bind(this);
 }
 
-Game.prototype.init = function() {
+Game.prototype._init = function() {
+	window.shadow = new Shadow(true);
+	window.ivan = new Ivan(true);
 
+	World.addPeople(shadow, ivan);
+	World.joinTeam('A', shadow);
+	World.joinTeam('B', ivan);
+
+	console.log(World.PEOPLE);
+
+	ivan.walk(new Point(20, 20));
+
+	//document.body.appendChild(shadow.thumb);
+	//document.body.appendChild(ivan.thumb);
+};
+
+Game.prototype._onHashChange = function(e) {
+	var hash = location.hash.substr(2);
+	var parts = hash.split("/");
+
+	if(parts.length != 2) return;
+
+	var worldResrc 	= parts[0];
+	var id 			= parts[1];
+
+	switch(worldResrc) {
+		case 'people':
+			var person = World.getPerson(id);
+			console.log(person);
+			break;
+	}
 };
